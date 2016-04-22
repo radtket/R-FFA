b723fb70b9df7811cd74d271b759156a60535c49
Taylor Radtke <taylorradtke@gmail.com>
2016-04-22 16:50:47 -0400

    darude

--- js/main.js
+++ js/main.js
@@ -46,6 +46,24 @@ $(document).ready(function(){
 			$('.navigation li').addClass('small-padding');
 	   }
 	});
+
+
+
+
+$("#nav-two a")
+  .each(function(i) {
+    if (i != 0) {
+      $("#beep-two")
+        .clone()
+        .attr("id", "beep-two" + i)
+        .appendTo($(this).parent());
+    }
+    $(this).data("beeper", i);
+  })
+  .mouseenter(function() {
+    $("#beep-two" + $(this).data("beeper"))[0].play();
+  });
+$("#beep-two").attr("id", "beep-two0");
   
   
 });
